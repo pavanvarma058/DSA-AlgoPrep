17f148492b55550b65719188d85f09630cb9a76d
Pavan Varma <pavanvarma058@gmail.com>
2025-04-04 16:42:49 +0530

    Learned about characters

--- 4-Strings/code/src/Character.java
+++ 4-Strings/code/src/Character.java
@@ -1,5 +1,14 @@
+import java.util.*;
 public class Character {
     public static void main(String[] args) {
-        
+        // Character
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Character: ");
+        char ch1 = sc.nextLine().charAt(0);
+        System.out.println(ch1);
+
+        //syntax
+        char ch2 = 'B';
+
     }
 }
