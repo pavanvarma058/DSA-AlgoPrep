b608b130c7fc94b42b3c9f3d1969b3e51cdf2459
Pavan Varma <pavanvarma058@gmail.com>
2025-04-08 13:19:16 +0530

    learned about binary number system and conversion of binary to decimal and vice versa

--- 5-Bit-Manipulation/code/src/Main.java
+++ 5-Bit-Manipulation/code/src/Main.java
@@ -1,17 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Binary number system
+        // {0 1}
+        // Decimal number system
+        // {0, 9}
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // To convert decimal to binary we should divide given number by 2
+        // until we get 0 as remainder
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // To convert binary to decimal we use bit index
+        // ex:    1      0         1           0           1
+        //     1*2^4   0*2^3      1*2^2     0*2^1       1*2^0
+        //       16  +   0     +    4     +    0     +    1   = 21
     }
 }
